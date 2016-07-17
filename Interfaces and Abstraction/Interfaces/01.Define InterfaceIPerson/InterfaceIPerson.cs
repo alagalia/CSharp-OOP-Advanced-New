765a548b787ab4ffb4b67958b9149d16ad4a50ca
Galya Georgieva <alice.gospodinova@gmail.com>
2016-07-17 12:35:41 +0300

    Interfaces

---
+++ Interfaces and Abstraction/Interfaces/01.Define InterfaceIPerson/InterfaceIPerson.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Define_InterfaceIPerson
+{
+    using System.Reflection;
+
+    class Program
+    {
+        static void Main()
+        {
+            Type personInterface = typeof(Citizen).GetInterface("IPerson");
+            PropertyInfo[] properties = personInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            IPerson person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+
+    public interface IPerson
+    {
+        string Name { get; }
+        int Age { get; }
+    }
+
+    public class Citizen : IPerson
+    {
+        private string name;
+
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name => this.name;
+
+        public int Age => this.age;
+    }
+}
