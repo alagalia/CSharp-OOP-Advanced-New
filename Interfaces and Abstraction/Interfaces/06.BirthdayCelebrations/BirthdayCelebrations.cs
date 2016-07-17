1cfc5ae7413897e506049189153b17e043452b33
Galya Georgieva <alice.gospodinova@gmail.com>
2016-07-17 15:05:29 +0300

    BirthdayCelebrations exercise

---
+++ Interfaces and Abstraction/Interfaces/06.BirthdayCelebrations/BirthdayCelebrations.cs
@@ -0,0 +1,40 @@
+﻿namespace _06.BirthdayCelebrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BirthdayCelebrations
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            List<IUnit> units = new List<IUnit>();
+            while (input != "End")
+            {
+                string[] tokens = input.Split();
+                if (tokens[0] != "Robot")
+                {
+                    if (tokens.Length == 5)
+                    {
+                        Citizen citizen = new Citizen(tokens[1], tokens[4]);
+                        units.Add(citizen);
+                    }
+                    else
+                    {
+                        Pet pet = new Pet(tokens[1], tokens[2]);
+                        units.Add(pet);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string year = Console.ReadLine();
+
+            List<IUnit> illegal = units.Where(u => u.Birthdate.EndsWith(year)).ToList();
+
+            illegal.ForEach(u => Console.WriteLine(u.Birthdate));
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Interfaces/06.BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,16 @@
+﻿namespace _06.BirthdayCelebrations
+{
+    public class Citizen : IUnit
+    {
+        public Citizen(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; set; }
+
+        public string Birthdate { get; }
+    }
+
+}
