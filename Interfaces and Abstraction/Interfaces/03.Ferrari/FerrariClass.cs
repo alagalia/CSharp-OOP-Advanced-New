988e513e68d212196f76621d5521bd534954b630
Galya Georgieva <alice.gospodinova@gmail.com>
2016-07-21 16:48:26 +0300

    03. Ferrari

---
+++ Interfaces and Abstraction/Interfaces/03.Ferrari/FerrariClass.cs
@@ -0,0 +1,52 @@
+﻿namespace _03.Ferrari
+{
+    using System;
+    class FerrariClass
+    {   
+        static void Main()
+        {
+            string driver = Console.ReadLine();
+            Ferrari f = new Ferrari(driver);
+            Console.WriteLine($"{Ferrari.Model}/{f.Brakes()}/{f.Gas()}/{f.Name}");
+
+            string ferrariName = typeof(Ferrari).Name;
+            string iCarInterfaceName = typeof(ICar).Name;
+
+            bool isCreated = typeof(ICar).IsInterface;
+            if (!isCreated)
+            {
+                throw new Exception("No interface ICar was created");
+            }
+        }
+    }
+
+    public interface ICar
+    {
+        string Brakes();
+
+        string Gas();
+    }
+
+    public class Ferrari : ICar
+    {
+        public const string Model = "488-Spider";
+        private string name;
+
+        public Ferrari(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public string Brakes()
+        {
+            return "Brakes!";
+        }
+
+        public string Gas()
+        {
+            return "Zadu6avam sA!";
+        }
+    }
+}
