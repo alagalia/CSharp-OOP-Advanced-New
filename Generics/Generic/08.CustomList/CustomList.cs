33247bf063a0c7d22e4e9435630e6421012553e2
Galya Georgieva <alice.gospodinova@gmail.com>
2016-07-24 11:03:52 +0300

    Custom List

---
+++ Generics/Generic/08.CustomList/CustomList.cs
@@ -0,0 +1,74 @@
+﻿namespace _08.CustomList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CustomList<T>
+        where T : IComparable
+    {
+        private List<T> elements;
+
+        public CustomList()
+        {
+            this.elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+
+        public T Remove(int index)
+        {
+            this.ValidateIndex(index);
+            T removed = this.elements[index];
+            this.elements.RemoveAt(index);
+            return removed;
+        }
+
+        public bool Contains(T element)
+        {
+            return this.elements.Contains(element);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            this.ValidateIndex(firstIndex, secondIndex);
+            T temp = this.elements[firstIndex];
+            this.elements[firstIndex] = this.elements[secondIndex];
+            this.elements[secondIndex] = temp;
+        }
+
+        public int CountGreaterThan(T element)
+        {
+            return this.elements.Count(e => e.CompareTo(element) > 0);
+        }
+
+        public T Max()
+        {
+            return this.elements.Max();
+        }
+
+        public T Min()
+        {
+            return this.elements.Min();
+        }
+
+        public void Print()
+        {
+            this.elements.ForEach(e => Console.WriteLine(e));
+        }
+
+        private void ValidateIndex(params int[] indices)
+        {
+            foreach (int index in indices)
+            {
+                if (index >= this.elements.Count || index < 0)
+                {
+                    throw new IndexOutOfRangeException("Index is outside of range!");
+                }
+            }
+        }
+    }
+}
---
+++ Generics/Generic/08.CustomList/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _08.CustomList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CustomList<string> myCollection = new CustomList<string>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                CommandExecutor(myCollection, input);
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void CommandExecutor(CustomList<string> myCollection, string input)
+        {
+            string[] tokens = input.Split();
+            switch (tokens[0])
+            {
+                case "Add":
+                    {
+                        var element = tokens[1];
+                        myCollection.Add(element);
+                        break;
+                    }
+
+                case "Remove":
+                    {
+                        int index = int.Parse(tokens[1]);
+                        myCollection.Remove(index);
+                        break;
+                    }
+
+                case "Contains":
+                    {
+                        var element = tokens[1];
+                        Console.WriteLine(myCollection.Contains(element));
+                        break;
+                    }
+
+                case "Swap":
+                    {
+                        int firstIndex = int.Parse(tokens[1]);
+                        int secondIndex = int.Parse(tokens[2]);
+                        myCollection.Swap(firstIndex, secondIndex);
+                        break;
+                    }
+
+                case "Greater":
+                    {
+                        var element = tokens[1];
+                        Console.WriteLine(myCollection.CountGreaterThan(element));
+                        break;
+                    }
+
+                case "Max":
+                    {
+                        Console.WriteLine(myCollection.Max());
+                        break;
+                    }
+
+                case "Min":
+                    {
+                        Console.WriteLine(myCollection.Min());
+                        break;
+                    }
+
+                case "Print":
+                    {
+                        myCollection.Print();
+                        break;
+                    }
+            }
+        }
+    }
+}
